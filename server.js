8174238c8cff067eb1d036bfcdde86fb6aa002a8
Dunia Goncalves <duniaimara@gmail.com>
2025-04-06 16:33:09 -0400

    add noo watch and PORT variable

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const corsOptions = {
-    origin: 'http://localhost:5173',
-    optionsSuccessStatus: 200
+  origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200
 }
 
+const app = express();
 app.use(cors(corsOptions));
 
 app.get('/api', (req, res) => {
   res.json({ moods: ['happy', 'sad', 'angry', 'excited'] });
 });
 
-app.listen(8787, () => {
-  console.log('Server running on http://localhost:8787');
+const PORT = 3001
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
